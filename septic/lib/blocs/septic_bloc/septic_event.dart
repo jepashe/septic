db1a6549f7ac89d45c4a3aa2fc5ccfc67ed56f68
jepashe <subochev.pavel@gmail.com>
2023-06-23 12:22:44 +0500

    новый блок для работы со септиком

---
+++ septic/lib/blocs/septic_bloc/septic_event.dart
@@ -0,0 +1,5 @@
+part of 'septic_bloc.dart';
+
+abstract class SepticEvent {}
+
+class SepticInitEvent extends SepticEvent {}
---
+++ septic/lib/blocs/septic_bloc/septic_state.dart
@@ -0,0 +1,8 @@
+part of 'septic_bloc.dart';
+
+abstract class SepticState extends Equatable {}
+
+class SepticInitState extends SepticState {
+  @override
+  List<Object?> get props => [];
+}
