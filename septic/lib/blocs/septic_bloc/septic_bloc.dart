7e55193dfc90d653309463c4b0d06922a8f856e5
jepashe <subochev.pavel@gmail.com>
2023-10-30 16:46:19 +0500

    начинаю писать блок для септика

--- septic/lib/blocs/septic_bloc/septic_bloc.dart
+++ septic/lib/blocs/septic_bloc/septic_bloc.dart
@@ -1,24 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:septic/domain/septic_repository.dart';
-import 'package:septic/domain/store_repository.dart';
 import 'package:equatable/equatable.dart';
+import 'package:septic/entity/user.dart';
 
 part 'septic_event.dart';
 part 'septic_state.dart';
 
 class SepticBloc extends Bloc<SepticEvent, SepticState> {
-  SepticBloc(
-      {required SepticRepository septicRepository,
-      required StoreRepository storeRepository})
+  SepticBloc({required SepticRepository septicRepository, required User user})
       : _septicRepository = septicRepository,
-        _storeRepository = storeRepository,
+        _currentUser = user,
         super(SepticInitState()) {
     on<SepticInitEvent>(_onSepticInit);
   }
 
   final SepticRepository _septicRepository;
-  final StoreRepository _storeRepository;
+  final User _currentUser;
 
-  _onSepticInit(SepticInitEvent event, Emitter<SepticState> emit) async {}
-
+  _onSepticInit(SepticInitEvent event, Emitter<SepticState> emit) async {
+    _septicRepository.getUserDevices();
+  }
 }
